6dfc44216e1948df1e040a68ac76cbeeae69d5c5
Pooriya <rahimzadehpooriya@yahoo.com>
2023-06-23 17:05:09 +0330

    v1.0.0

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(requests):
+    return render(requests, 'website/index.html')
+
+
+def blog_view(requests):
+    return render(requests, 'blog/blog.html')
