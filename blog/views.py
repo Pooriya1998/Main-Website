86acd1d37a144f2263ff7ed92725ab3d859d4487
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-06-30 17:53:49 +0330

    Making the blog section dynamic on the website

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
-# Create your views here.
+
+def blog_view(requests):
+    posts = Post.objects.filter(status=1)
+    context = {'posts' : posts}
+    return render(requests, 'blog/blog.html', context)
--- website/views.py
+++ website/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def index_view(requests):
     return render(requests, 'website/index.html')
 
-
-def blog_view(requests):
-    return render(requests, 'blog/blog.html')
