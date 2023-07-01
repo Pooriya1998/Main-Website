342043c079086e46d21bafeb8508e8603f0ea7aa
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-07-01 11:14:01 +0330

    Create a status change section for blog articles

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,19 @@ import datetime
 
 
 def blog_view(requests):
-    posts = Post.objects.exclude(published_date__gt=datetime.datetime.now())
-    context = {'posts': posts}
+    all_posts = Post.objects.all()
+    published_posts = Post.objects.exclude(published_date__gt=datetime.datetime.now())
+    unpublished_posts = Post.objects.filter(published_date__gt=datetime.datetime.now())
+
+    for post in all_posts:
+        if post in published_posts:
+            post.status = True
+            post.save()
+        else:
+            post.status = False
+            post.save()
+
+    context = {'published_posts': published_posts, 'unpublished_posts' : unpublished_posts}
     return render(requests, 'blog/blog.html', context)
 
 
