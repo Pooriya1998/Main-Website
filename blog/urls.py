150d71e86493175d5621903bbe14f1d35df1133f
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-07-08 10:58:03 +0330

    Setting up the category section

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name='index'),
     path('<int:pid>', blog_single_view, name='single'),
+    path('category/<str:cat_name>/', blog_category_view, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from blog.models import Post, Category
 import datetime
 
 
-def blog_view(requests):
+def blog_view(request):
     all_posts = Post.objects.all()
     published_posts = Post.objects.exclude(published_date__gt=datetime.datetime.now())
     unpublished_posts = Post.objects.filter(published_date__gt=datetime.datetime.now())
@@ -18,16 +18,20 @@ def blog_view(requests):
             post.save()
 
     context = {'published_posts': published_posts, 'unpublished_posts': unpublished_posts, 'all_cats': all_cats}
-    return render(requests, 'blog/blog.html', context)
+    return render(request, 'blog/blog.html', context)
 
 
-def blog_single_view(requests, pid):
+def blog_single_view(request, pid):
     post = get_object_or_404(Post, pk=pid, status=1)
     all_cats = Category.objects.all()
     post.counted_views += 1
     post.save()
     context = {'post': post, 'all_cats': all_cats}
-    return render(requests, 'blog/blog-single.html', context)
+    return render(request, 'blog/blog-single.html', context)
 
 
-
+def blog_category_view(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog.html', context)
