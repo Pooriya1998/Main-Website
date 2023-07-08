6321e3ea2398b722ebc4f4a0f5c3c09381698b60
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-07-08 11:18:14 +0330

    Add the WebSocket protocol to the project

--- main_website/asgi.py
+++ main_website/asgi.py
@@ -2,10 +2,11 @@ import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_website.settings')
 
 application = ProtocolTypeRouter({
 
-    "http" : get_asgi_application(),
+    "http": get_asgi_application(),
 
 })
