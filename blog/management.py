0ced1ee79ef94ea33df09769dc186ce87bc94bdc
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-07-09 07:18:02 +0330

    1. Fixed the problem of not filtering the categories on the display on the site 2. Optimizing the codes

--- blog/management.py
+++ blog/management.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from blog.models import Post, Category
+from django.shortcuts import get_object_or_404
+from blog.models import Post
 import datetime
 
 
