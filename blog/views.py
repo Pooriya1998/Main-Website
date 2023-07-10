eb5b3cd337b5257b21edc4109f4d41eaca8aa4fb
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-07-10 13:16:44 +0330

    1. Setting the pagination section in the blog 2. Adjusting the search section in the blog 3. Setting the filtering section based on the author in the blog 4. Updating the view section in the blog

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,47 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.management import *
 import datetime
 
 
-def blog_view(request):
-
+def blog_view(request, **kwargs):
+    global page_number
     check_status()
 
-    published_posts = Post.objects.exclude(published_date__gt=datetime.datetime.now())
-    all_cats = Category.objects.all()
+    posts = Post.objects.exclude(published_date__gt=datetime.datetime.now())
+    if kwargs.get('cat_name') is not None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') is not None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts, 3)
 
-    context = {'posts': published_posts, 'all_cats': all_cats}
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+
+    all_cats = Category.objects.all()
+    context = {'posts': posts, 'all_cats': all_cats}
     return render(request, 'blog/blog.html', context)
 
 
 def blog_single_view(request, pid):
-
     add_counted_views(pid)
-
     post = get_object_or_404(Post, pk=pid, status=1)
     all_cats = Category.objects.all()
-
     context = {'post': post, 'all_cats': all_cats}
     return render(request, 'blog/blog-single.html', context)
 
 
-def blog_category_view(request, cat_name):
+def blog_search(request):
     posts = Post.objects.filter(status=1)
-    posts = posts.filter(category__name=cat_name)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+
     context = {'posts': posts}
     return render(request, 'blog/blog.html', context)
